ab8e1e4cf8ff4f55d4aa2ba05dd2922b3b17fae6
Sandy3122 <seeram87@gmail.com>
2023-11-02 17:56:58 +0530

    /view-statement route has been added.

--- database.js
+++ database.js
@@ -369,7 +369,7 @@ app.post('/make-payment/:customer_id/:loan_id', async (req, res) => {
     }
 
     // Fetch loan details
-    const [loanData] = await pool.query(
+    const [loanData] = await pool.execute(
       'SELECT * FROM loans WHERE loan_id = ? AND customer_id = ?',
       [loan_id, customer_id]
     );
@@ -408,7 +408,7 @@ app.post('/make-payment/:customer_id/:loan_id', async (req, res) => {
     }
 
     // Update the loan with the new remaining loan amount, tenure, and number_of_successful_payments
-    await pool.query(
+    await pool.execute(
       'UPDATE loans SET remaining_loan_amount = ?, remaining_tenure = ?, successful_payments = ? WHERE loan_id = ? AND customer_id = ?',
       [updatedRemainingLoanAmount, updatedTenure, numberOfPayments + paymentAmount / monthlyPayment, loan_id, customer_id] // Increment successful_payments
     );
@@ -426,8 +426,41 @@ app.post('/make-payment/:customer_id/:loan_id', async (req, res) => {
   }
 });
 
+app.get('/view-statement/:customer_id/:loan_id', async (req, res) => {
+  try {
+    const customer_id = req.params.customer_id;
+    const loan_id = req.params.loan_id;
 
+    // Fetch loan details
+    const [loanData] = await pool.execute(
+      'SELECT * FROM loans WHERE loan_id = ? AND customer_id = ?',
+      [loan_id, customer_id]
+    );
 
+    if (loanData.length === 0) {
+      return res.status(404).json({ error: 'Loan not found' });
+    }
+
+    const loan = loanData[0];
+    const amountPaid = loan.successful_payments * loan.monthly_payment;
+    const repaymentsLeft = loan.remaining_tenure
+
+    const statement = {
+      customer_id: loan.customer_id,
+      loan_id: loan.loan_id,
+      principal: loan.loan_amount,
+      interest_rate: loan.interest_rate,
+      amount_paid: amountPaid,
+      monthly_installment: loan.monthly_payment,
+      repayments_left: repaymentsLeft,
+    };
+
+    return res.status(200).json(statement);
+  } catch (error) {
+    console.error('Database query error:', error);
+    res.status(500).json({ error: 'An error occurred while processing your request.' });
+  }
+});
 
 
 
